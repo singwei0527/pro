59410aeef7ff4f08d08344ec28de2c80e477462d
singwei0527 <42398438+singwei0527@users.noreply.github.com>
2018-12-12 21:43:57 +0800

    Add files via upload

---
+++ class18/server.js
@@ -0,0 +1,36 @@
+var express = require("express")
+var http = express();
+var Db = require("./mongodbFZ.js")
+var db = new Db("randio");
+http.listen(8080,()=>{
+	console.log("OK");
+})
+http.use((req,res,next)=>{
+	res.header("Access-Control-Allow-Origin","*")
+	next();
+})
+
+http.get("/add",(req,res)=>{
+	var data = req.query;
+	db.find("list",{query:{wish:data.wish}},(err,data1)=>{
+		if(data1.length==0){
+	        data.time = new Date().getTime();
+			db.insertOne("list",data,(err,data2)=>{
+				res.send(data2)
+			})
+		}else{
+			res.send("不能重复许愿");
+		}
+	})
+})
+http.get("/show",(req,res)=>{
+	db.find("list",{},(err,data)=>{
+		res.send(data);
+	})
+})
+http.get("/del",(req,res)=>{
+	var id = req.query.id
+	db.deleteById("list",id,(err,data)=>{
+		res.send(data);
+	})
+})
